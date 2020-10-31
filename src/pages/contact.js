2d43c56e9c25ee7c697a76acf8ec2a261f589282
Marek Reid <m@Mareks-MBP.fritz.box>
2020-10-31 22:14:47 +0100

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+import ContactPage from "../components/pages/contact/Contact"
+
+function Contact() {
+  return (
+    <Layout>
+      <ContactPage />
+    </Layout>
+  )
+}
+
+export default Contact
