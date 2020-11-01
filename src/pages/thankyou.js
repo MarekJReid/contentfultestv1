ca12b595a9c0878624123da30ea4b3d992fe4b4d
Marek Reid <m@Mareks-MBP.fritz.box>
2020-11-01 06:45:22 +0100

    added state to ty page

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -3,10 +3,9 @@ import Layout from "../components/layout"
 import "../App.scss"
 import img from '../images/thankyou-landing.jpg'
 function ThankYou({location}) {
-//  let type = location.state.type
-//  let name = location.state.name
-let state = location.state
-console.log(state)
+ let type = location.state.type
+ let name = location.state.name
+
   return (
     <Layout>
       <div className="thankyou-page">
@@ -15,24 +14,23 @@ console.log(state)
             <img src={img} alt=""/>
           </div>
           <div class="text-container">
-            Thank you very much!
-            {/* <h1> */}
-            {/* {type != "enquiry" ? (
+            <h1>
+            {type != "enquiry" ? (
               <h3>Thank you very much for subscribing to our mailing list {name} ! </h3>
             )
           :
           (
-            <h3>Thank you very much for your enquiry {name}! </h3>
+            <h1>Thank you very much for your enquiry {name}! </h1>
           )
-          } */}
-            {/* </h1>
+          }
+            </h1>
             {type != "enquiry" ? (<h3>
             You will be notified with all of the latest news and releases
             as soon as they come to light.
           </h3>
 
             ) :  <h3>We will get back to you as soon as possible.</h3>}
-            */}
+           
           </div>
         </div>
       </div>
